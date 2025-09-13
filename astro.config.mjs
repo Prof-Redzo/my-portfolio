4ee5e0c8609924e7ddc4684431da14c54ca28834
Redžo <154461862+Prof-Redzo@users.noreply.github.com>
2025-09-13 09:02:20 +0200

    Add fix.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: 'https://<Prof-Redzo>.github.io',
+  site: 'https://<prof-redzo>.github.io',
   base: '/<my-portfolio>',       
 });
